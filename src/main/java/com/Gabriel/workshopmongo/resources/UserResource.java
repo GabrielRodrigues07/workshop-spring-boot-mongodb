9a3ffbab4be87c0594b84cf3f552f33c2c627d6b
Gabriel Rodrigues <grodrigues256@gmail.com>
2021-04-08 20:20:10 -0300

    Deleção de usuário com DELETE

--- src/main/java/com/Gabriel/workshopmongo/resources/UserResource.java
+++ src/main/java/com/Gabriel/workshopmongo/resources/UserResource.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +18,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.Gabriel.workshopmongo.domain.User;
 import com.Gabriel.workshopmongo.dto.UserDTO;
 import com.Gabriel.workshopmongo.services.UserService;
+import com.Gabriel.workshopmongo.services.exception.ObjectNotFoundException;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -44,5 +46,14 @@ public class UserResource {
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable String id) {
+		if(id == null) {
+			throw new ObjectNotFoundException("Objeto não existe!");
+		}
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }
